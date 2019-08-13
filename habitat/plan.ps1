2d6aae76695e2d88a80ae1baa86abb6b08b94470
Dan <danf425@gmail.com>
2019-08-13 15:56:17 +0000

    plan update. ps1 works, but not how it should. Need src to be preloaded

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -7,22 +7,9 @@
 # where the package is installed to on disk, how it is referred to in package
 # metadata, and so on.
 $pkg_name="adminator"
-
-# Required unless overridden by the `HAB_ORIGIN` environment variable.
-# The origin is used to denote a particular upstream of a package.
 $pkg_origin="danf"
-
-# Required.
-# Sets the version of the package
 $pkg_version="1.0.0"
-
-# Optional.
-# The name and email address of the package maintainer.
 $pkg_maintainer="Dan Flores dmontanez@chef.io"
-
-# Optional.
-# An array of valid software licenses that relate to this package.
-# Please choose a license from http://spdx.org/licenses/
 $pkg_license=@("Apache-2.0")
 $pkg_upsteam_url="https://github.com/danf425/Adminator-admin-dashboard"
 $pkg_description="Adminator NodeJS Dashboard Application"
@@ -33,49 +20,47 @@ $pkg_deps=@(
 )  
 $pkg_build_deps=@(
     "core/git"
-    "core/python"
 )
 
 # function Invoke-Begin {
 #  Invoke-DefaultBegin
-
 # }
-
 function Invoke-Download {
    Invoke-DefaultDownload
+  # Maybe download should be part of the process, but then it would be harder to edit 
   #  git clone https://github.com/danf425/Adminator-admin-dashboard.git adminator
+  #  cd adminator
   #  Get-ChildItem
   #  Start-Sleep -s 2
-  #  cd adminator
- #  Get-ChildItem
- #  Start-Sleep -s 2
 }
 # function Invoke-Verify {
 #   Invoke-DefaultVerify
 # }
-
 # function Invoke-Clean {
 #   Invoke-DefaultClean
 # }
-
 # function Invoke-Unpack {
 #   Invoke-DefaultUnpack
-# # }
+# }
 # function Invoke-Prepare {
 #   Invoke-DefaultPrepare
 # }
-
 function Invoke-Build {
   Invoke-DefaultBuild
 }
 # function Invoke-Check {}
 
 function Invoke-Install {
-  #Invoke-DefaultInstall  
-#   echo "version below"
+   echo "LOOK HERERERE HERHER1"
+   echo $pkg_svc_var_path
+   echo "LOOK HERERERE HERHE2"
+   echo $HAB_CACHE_SRC_PATH
+   echo "LOOK HERERERE HERHE3E"
+   echo $pkg_dirname
+#   echo "npm version below"
 #   npm -v
-#   echo "Get location"
-#   Get-Location
+   echo "Get location"
+   Get-Location
 #   Start-Sleep -s 2
 #   echo "list files:"
 # #  Get-ChildItem -Path "C:\hab\cache\src\adminator-1.0.0\"
@@ -83,16 +68,17 @@ function Invoke-Install {
 #   cd C:\src\habitat\adminator
 #   Get-Location
 #   Start-Sleep -s 2
-   git clone https://github.com/danf425/Adminator-admin-dashboard.git adminator
-   Get-ChildItem
-   Start-Sleep -s 2
-   cd adminator
+
+ #Donload repo and start process
+  git clone https://github.com/danf425/Adminator-admin-dashboard.git adminator
+  cd adminator
+  npm install
+  invoke-expression 'cmd /c start powershell -Command { write-host "Hi, new window!"; npm run dev  }'
+#  npm run dev
+
   # echo "move files"
   # Copy-Item ".\package.json" -Destinatio "C:\hab\cache\src\adminator-1.0.0\"
-  echo "npm install"
-  npm install
-  npm run dev
-#  npm run dev
+ 
 }
 
 # function Invoke-End {
